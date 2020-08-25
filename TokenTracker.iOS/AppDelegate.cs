887b27eab2e58487297c662dbeb4d8f83866d8dc
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-08-25 23:12:25 +0700

    Show white text in status bar for iOS

--- TokenTracker.iOS/AppDelegate.cs
+++ TokenTracker.iOS/AppDelegate.cs
@@ -22,7 +22,7 @@ namespace TokenTracker.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
+            Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
