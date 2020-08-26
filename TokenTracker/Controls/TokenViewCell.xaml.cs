08b1d1299562e5bf7524606039398207389517fa
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-08-26 14:47:20 +0700

    Improve styling of token search view cells

--- TokenTracker/Controls/TokenViewCell.xaml.cs
+++ TokenTracker/Controls/TokenViewCell.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TokenTracker.Models;
+﻿using TokenTracker.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
