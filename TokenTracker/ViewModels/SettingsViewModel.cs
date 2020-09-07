9608d7b2639e94bb693d03d4fd88c275fb7c1e62
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-09-07 22:03:52 +0700

    Update icons

--- TokenTracker/ViewModels/SettingsViewModel.cs
+++ TokenTracker/ViewModels/SettingsViewModel.cs
@@ -12,7 +12,7 @@ namespace TokenTracker.ViewModels
             new ChooserSettingItem { Title = "Currency", Choices = new List<string> { "USD", "EUR", "BTC" }, SelectedChoiceIndex = 0, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_currency_b.png") },
             new ChooserSettingItem { Title = "Theme", Choices = new List<string> { "Light", "Dark", "Novel" }, SelectedChoiceIndex = 0, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_theme_b.png") },
             new ChooserSettingItem { Title = "Sort By", Choices = new List<string> { "Alphabet", "Rank" }, SelectedChoiceIndex = 0, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_sort_b.png") },
-            new SwitchSettingItem { Title = "Disable Standby", IsSelected = false, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_power_b.png") },
+            new SwitchSettingItem { Title = "Disable Standby", IsSelected = false, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_standby_b.png") },
             new LabelSettingItem { Title = "Disable Ads", IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_block_b.png") },
             new LabelSettingItem { Title = "Clear Cache", IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_history_b.png") },
         };
