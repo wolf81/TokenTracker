82a69b8a925d0f74137902c882e814c3a8ec87b2
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-09-07 18:19:42 +0700

    Add icons for the settings screen and additional setting options

---
+++ TokenTracker/Utilities/SettingTemplateSelector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TokenTracker
+{
+    public abstract class SettingItemBase
+    {
+        public ImageSource IconImageSource { get; set; }
+
+        public string Title { get; set; }
+    }
+
+    public class ChooserSettingItem : SettingItemBase
+    {
+        public int SelectedChoiceIndex { get; set; } = 0;
+
+        public List<string> Choices { get; set; } = new List<string> { "Choice 1" };
+    }
+
+    public class SwitchSettingItem : SettingItemBase
+    {
+        public bool IsSelected { get; set; } = false;
+    }
+
+    public class LabelSettingItem: SettingItemBase { }
+
+    public class SettingTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ChooserTemplate { get; set; }
+
+        public DataTemplate SwitchTemplate { get; set; }
+
+        public DataTemplate LabelTemplate { get; set; }
+        
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is SwitchSettingItem)
+            {
+                return SwitchTemplate;
+            }
+            else if (item is ChooserSettingItem)
+            {
+                return ChooserTemplate;
+            }
+            else if (item is LabelSettingItem)
+            {
+                return LabelTemplate;
+            }
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- TokenTracker/ViewModels/SettingsViewModel.cs
+++ TokenTracker/ViewModels/SettingsViewModel.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using TokenTracker.ViewModels.Base;
+using Xamarin.Forms;
 
 namespace TokenTracker.ViewModels
 {
     public class SettingsViewModel : ViewModelBase
     {
-        private List<string> items = new List<string> { "Hi" };
-        public List<string> Items
+        private List<SettingItemBase> items = new List<SettingItemBase>
+        {
+            new ChooserSettingItem { Title = "Currency", Choices = new List<string> { "USD", "EUR", "BTC" }, SelectedChoiceIndex = 0, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_currency_b.png") },
+            new ChooserSettingItem { Title = "Theme", Choices = new List<string> { "Light", "Dark" }, SelectedChoiceIndex = 0, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_theme_b.png") },
+            new ChooserSettingItem { Title = "Sort By", Choices = new List<string> { "Alphabet", "Rank" }, SelectedChoiceIndex = 0, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_sort_b.png") },
+            new SwitchSettingItem { Title = "Disable Sleep", IsSelected = false, IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_power_b.png") },
+            new LabelSettingItem { Title = "Disable Ads", IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_block_b.png") },
+            new LabelSettingItem { Title = "Clear Cache", IconImageSource = ImageSource.FromResource("TokenTracker.Resources.ic_history_b.png") },
+        };
+
+        public List<SettingItemBase> Items
         {
             get => items;
             set => SetProperty(ref items, value);
