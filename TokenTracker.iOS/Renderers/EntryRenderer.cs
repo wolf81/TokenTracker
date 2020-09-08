9781e07f8926de07cafb03ef893cecb7b2f8e01f
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-09-08 15:03:36 +0700

    Add custom entry renderer for iOS to disable auto-correct, auto-capitalize & spell checking

---
+++ TokenTracker.iOS/Renderers/EntryRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(TokenTracker.Renderers.EntryRenderer))]
+namespace TokenTracker.Renderers
+{
+    public class EntryRenderer :  Xamarin.Forms.Platform.iOS.EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control is UITextField textField)
+            {
+                textField.AutocorrectionType = UITextAutocorrectionType.No;
+                textField.SpellCheckingType = UITextSpellCheckingType.No;
+                textField.AutocapitalizationType = UITextAutocapitalizationType.None;
+            }
+        }
+    }
+}
